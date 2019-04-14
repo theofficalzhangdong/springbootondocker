67905078b7275c6eaef22bbbe386a7dec205411d
zhangdong <1002039155@qq.com>
2019-04-14 16:40:40 +0800

    test1

---
+++ src/main/java/com/example/mydocker/Controller/MyDockerController.java
@@ -0,0 +1,19 @@
+package com.example.mydocker.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * the controller of this program
+ *
+ */
+@RestController
+public class MyDockerController {
+
+    @RequestMapping("/hellodocker")
+    @ResponseBody
+    public String sayHelloToDocker() {
+        return "this spring boot program is running on docker.";
+    }
+}
---
+++ src/main/java/com/example/mydocker/MydockerApplication.java
@@ -0,0 +1,19 @@
+package com.example.mydocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+/**
+ * the main of this program
+ *
+ */
+@SpringBootApplication
+public class MydockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydockerApplication.class, args);
+	}
+
+}
