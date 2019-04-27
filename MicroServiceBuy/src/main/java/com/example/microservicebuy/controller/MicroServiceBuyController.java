6cf2a41559a4a066db20b63386793bf3b8a524c9
zhangdong <1002039155@qq.com>
2019-04-27 14:36:35 +0800

    fix buy controller restcontroller

--- MicroServiceBuy/src/main/java/com/example/microservicebuy/controller/MicroServiceBuyController.java
+++ MicroServiceBuy/src/main/java/com/example/microservicebuy/controller/MicroServiceBuyController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value="buy")
+@RestController(value="/buy")
 public class MicroServiceBuyController {
     @RequestMapping("/ball")
     @ResponseBody
